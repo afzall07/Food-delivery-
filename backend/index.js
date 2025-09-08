669c97fb425200f68eea54c853ac482566b5270a
afzall07 <afjalmalik75000@gmail.com>
2025-09-08 16:19:47 +0530

    changes in index.js file

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,38 @@
-import express, { json } from 'express'
+import express from 'express'
 import dotenv from 'dotenv'
 dotenv.config()
 import connectDb from './config/db.js'
 import cookieParser from 'cookie-parser'
 import authRouter from './routes/auth.routes.js'
 import cors from "cors"
+
 const app = express()
 const port = process.env.PORT || 5000
+
+// Middlewares
 app.use(cors({
     origin: "http://localhost:5173",
-    credentials: true, // for parse cookies
+    credentials: true,
 }))
 app.use(express.json())
 app.use(cookieParser())
+
+// Routes
 app.use("/api/auth", authRouter)
-app.listen(port, () => {
-    connectDb()
-    console.log(`server started at ${port}`)
-})
+
+// Start server
+const startServer = async () => {
+    try {
+        await connectDb(); // yahan DB ka console handle hoga
+
+        app.listen(port, () => {
+            console.log(`Server started at port ${port}`);
+        });
+    } catch (error) {
+        console.error("Server not started because DB connection failed");
+        process.exit(1);
+    }
+};
+
+startServer();
+
