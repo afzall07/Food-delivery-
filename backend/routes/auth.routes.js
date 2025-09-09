2836ef97725de16931a21c882b33caf7f191e347
afzall07 <afjalmalik75000@gmail.com>
2025-09-09 16:09:06 +0530

    routing

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -4,6 +4,7 @@ import { signUp, signIn, logOut } from '../controllers/auth.controllers.js'
 const authRouter = express.Router()
 
 authRouter.post("/signup", signUp)
+
 authRouter.post("/signin", signIn)
 authRouter.get("/logout", logOut)
 
