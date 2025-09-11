e78770d7f7508f84be9d1bd1a1420c0e0941db65
afzall07 <afjalmalik75000@gmail.com>
2025-09-11 16:32:50 +0530

    reset  password contoller

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,6 +1,7 @@
 import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
 import genToken from "../utils/token.js";
+import { sendOtpMail } from "../utils/mail.js";
 
 //signup controller
 export const signUp = async (req, res) => {
@@ -90,5 +91,61 @@ export const logOut = async (req, res) => {
         return res.status(200).json({ message: "log Out successfully" })
     } catch (error) {
         return res.status(500).json(`log Out error ${error}`)
+    }
+}
+
+// OTP send controller
+export const sendOtp = async (req, res) => {
+    try {
+        const { email } = req.body
+        const user = await User.findOne({ email })
+        if (!user) {
+            return res.status(400).json({ message: "User doesn't exist." });
+        }
+        const otp = Math.floor(1000 + Math.random() * 9000).toString()
+        user.resetOtp = otp
+        user.otpExpires = Date.now() + 5 * 60 * 1000
+        user.isOtpVerified = false
+        await user.save()
+        await sendOtpMail(email, otp)
+        return res.status(200).json({ message: "OTP sent successfully" });
+    } catch (error) {
+        return res.status(500).json(`otp send error ${error}`)
+    }
+}
+
+// OTP verify controller
+export const verifyOtp = async (req, res) => {
+    try {
+        const { email, otp } = req.body
+        const user = await User.findOne({ email })
+        if (!user || user.resetOtp != otp || user.otpExpires > Date.now()) {
+            return res.status(400).json({ message: "invalid/expired OTP" })
+        }
+        user.resetOtp = undefined
+        user.otpExpires = undefined
+        user.isOtpVerified = true
+        await user.save()
+        return res.status(200).json({ message: "OTP verified successfully" })
+    } catch (error) {
+        return res.status(500).json(`OTP verify error ${error}`)
+    }
+}
+
+// reset Password controller
+export const resetPassword = async (req, res) => {
+    try {
+        const { email, newPassword } = req.body
+        const user = await User.findOne({ email })
+        if (!user || !user.isOtpVerified) {
+            return res.status(400).json({ message: "OTP verification required" })
+        }
+        const hashedPassword = await bcrypt.hash(newPassword, 10)
+        user.password = hashedPassword
+        user.isOtpVerified = false
+        await user.save()
+        return res.status(200).json({ message: 'password reset successfully' })
+    } catch (error) {
+        res.status(500).json(`reset password error ${error}`)
     }
 }
