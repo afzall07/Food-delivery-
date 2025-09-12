874abba8c40606781c5d68cbc8e500a9191a83db
afzall07 <afjalmalik75000@gmail.com>
2025-09-12 22:05:04 +0530

    password reset success

--- frontend/src/pages/ForgotPassword.jsx
+++ frontend/src/pages/ForgotPassword.jsx
@@ -1,16 +1,110 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import logo from "../images/bg-removed-logo.png";
 import { IoArrowBack } from "react-icons/io5";
-import { useNavigate } from "react-router-dom";
+import { data, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function ForgotPassword() {
   const [step, setStep] = useState(1);
   const [email, setEmail] = useState("");
   const [otp, setOtp] = useState("");
-  const [otpError, setOtpError] = useState("");
   const [newPassword, setNewPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const navigate = useNavigate();
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+  useEffect(() => {
+    setSuccess("");
+    setError("");
+  }, [step]);
+  // handle send otp
+  const handleSendOtp = async () => {
+    setError("");
+    setSuccess("");
+    try {
+      const result = await axios.post(
+        "http://localhost:7000/api/auth/send-otp",
+        {
+          email,
+        },
+        { withCredentials: true }
+      );
+      console.log(result.data.message);
+      setSuccess(result.data.message || "✅ Otp sent successfully");
+      setTimeout(() => {
+        setStep(2);
+      }, 3000);
+    } catch (error) {
+      console.error(error);
+      setError(error.response?.data?.message || "Something went wrong");
+    }
+  };
+
+  // handle verify otp
+  const handleVerfyOtp = async () => {
+    setError("");
+    setSuccess("");
+    try {
+      const result = await axios.post(
+        "http://localhost:7000/api/auth/verify-otp",
+        {
+          email,
+          otp,
+        },
+        { withCredentials: true }
+      );
+      console.log(result.data.message);
+      setSuccess(result.data.message || "✅ OTP verified successfully");
+      setTimeout(() => {
+        setStep(3);
+      }, 3000);
+    } catch (error) {
+      console.error(error);
+      setError(error.response?.data?.message || "invalid Otp");
+    }
+  };
+
+  // handle reset password
+  const handleResetPassword = async () => {
+    setError("");
+    setSuccess("");
+
+    const passwordRegex =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/;
+
+    if (!passwordRegex.test(newPassword)) {
+      setError(
+        "⚠️ Password must have 6+ chars, include uppercase, lowercase, number & symbol"
+      );
+      return;
+    }
+
+    if (newPassword !== confirmPassword) {
+      setError("⚠️ Password and Confirm Password do not match");
+      return;
+    }
+
+    try {
+      const result = await axios.post(
+        "http://localhost:7000/api/auth/reset-password",
+        {
+          email,
+          newPassword,
+        },
+        { withCredentials: true }
+      );
+      console.log(result.data.message);
+      setSuccess(result.data.message || "✅ Password changed successfully!");
+      setNewPassword("");
+      setConfirmPassword("");
+      setTimeout(() => {
+        navigate("/signin");
+      }, 3000);
+    } catch (err) {
+      setError(err.response?.data?.message || "Something went wrong");
+    }
+  };
+
   return (
     <div className="flex items-center justify-center min-h-screen p-4 bg-[#fff9f6]">
       <div className="bg-white rounded-xl shadow-lg w-full max-w-md p-8">
@@ -43,12 +137,17 @@ function ForgotPassword() {
                 placeholder="Enter your email"
                 onChange={(e) => {
                   setEmail(e.target.value);
+                  setError("");
                 }}
                 value={email}
               />
+              {error && <p className="text-red-600">{error}</p>}
+              {success && <p className="text-green-600">{success}</p>}
             </div>
+
             <button
               className={`w-full font-semibold py-2 rounded-lg transition duration-200 bg-[#ff4d2d] text-white hover:bg-[#e64323] cursor-pointer`}
+              onClick={handleSendOtp}
             >
               Send OTP
             </button>
@@ -73,10 +172,14 @@ function ForgotPassword() {
                 }}
                 value={otp}
               />
-              <span className="otp-error  text-red-600">{otpError}</span>
+              <div className="flex justify-between">
+                {error && <p className="text-red-600">{error}</p>}
+                {success && <p className="text-green-600">{success}</p>}
+              </div>
             </div>
             <button
               className={`w-full font-semibold py-2 rounded-lg transition duration-200 bg-[#ff4d2d] text-white hover:bg-[#e64323] cursor-pointer`}
+              onClick={handleVerfyOtp}
             >
               Verify
             </button>
@@ -101,6 +204,8 @@ function ForgotPassword() {
                 }}
                 value={newPassword}
               />
+              {error && <p className="text-red-600">{error}</p>}
+              {success && <p className="text-green-600">{success}</p>}
             </div>
             <div className="mb-4">
               <label
@@ -122,6 +227,7 @@ function ForgotPassword() {
             </div>
             <button
               className={`w-full font-semibold py-2 rounded-lg transition duration-200 bg-[#ff4d2d] text-white hover:bg-[#e64323] cursor-pointer`}
+              onClick={handleResetPassword}
             >
               Reset Password
             </button>
