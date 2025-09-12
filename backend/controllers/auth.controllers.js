4f0d3749d4d5504c449e12c4bf26f74082c4d9e4
afzall07 <afjalmalik75000@gmail.com>
2025-09-12 19:09:19 +0530

    set otp expire time

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -119,7 +119,7 @@ export const verifyOtp = async (req, res) => {
     try {
         const { email, otp } = req.body
         const user = await User.findOne({ email })
-        if (!user || user.resetOtp != otp || user.otpExpires > Date.now()) {
+        if (!user || user.resetOtp != otp || user.otpExpires < Date.now()) {
             return res.status(400).json({ message: "invalid/expired OTP" })
         }
         user.resetOtp = undefined
