80449195b9c3d13802863711a35c3fa7fb9f6630
afzall07 <afjalmalik75000@gmail.com>
2025-09-16 10:13:35 +0530

    add user router

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import connectDb from './config/db.js'
 import cookieParser from 'cookie-parser'
 import authRouter from './routes/auth.routes.js'
 import cors from "cors"
+import userRouter from './routes/user.routes.js'
 
 const app = express()
 const port = process.env.PORT || 5000
@@ -19,6 +20,7 @@ app.use(cookieParser())
 
 // Routes 
 app.use("/api/auth", authRouter)
+app.use("/api/user", userRouter)
 
 // Server start
 app.listen(port, async () => {
