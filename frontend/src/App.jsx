2b2579a2b04aaf8887fb56d050dc58a5c58deda0
afzall07 <afjalmalik75000@gmail.com>
2025-09-17 15:58:34 +0530

    create home route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,32 @@
 import { useState } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 import ForgotPassword from "./pages/ForgotPassword";
 import useGetCurrentUser from "./hooks/useGetCurrentUser";
+import { useSelector } from "react-redux";
+import Home from "./pages/Home";
 function App() {
   useGetCurrentUser();
+  const { userData } = useSelector((state) => state.user);
   return (
     <Routes>
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="/signin" element={<SignIn />} />
-      <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route
+        path="/"
+        element={userData ? <Home /> : <Navigate to={"/signin"} />}
+      />
+      <Route
+        path="/signup"
+        element={!userData ? <SignUp /> : <Navigate to={"/"} />}
+      />
+      <Route
+        path="/signin"
+        element={!userData ? <SignIn /> : <Navigate to={"/"} />}
+      />
+      <Route
+        path="/forgot-password"
+        element={!userData ? <ForgotPassword /> : <Navigate to={"/"} />}
+      />
     </Routes>
   );
 }
