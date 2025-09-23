e65bc68e89fea86d49cb31a88067ff7864725caf
afzall07 <afjalmalik75000@gmail.com>
2025-09-23 17:39:11 +0530

    shop routes

--- backend/routes/shop.routes.js
+++ backend/routes/shop.routes.js
@@ -1,8 +1,9 @@
 import express from 'express'
 import { createEditShop } from '../controllers/shop.controllers.js'
 import { isAuth } from '../middlewares/isAuth.js'
+import { upload } from '../middlewares/multer.js'
 
 const shopRouter = express.Router()
-shopRouter.get('/create-edit', isAuth, createEditShop)
+shopRouter.get('/create-edit', isAuth, upload.single("image"), createEditShop)
 
 export default shopRouter
