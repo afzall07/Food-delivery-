6cbd78c8ae19d8c7abdc96bb5d8f55266e62c010
afzall07 <afjalmalik75000@gmail.com>
2025-10-12 01:17:52 +0530

    add get shop by city route

--- backend/routes/shop.routes.js
+++ backend/routes/shop.routes.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { createEditShop, getShop } from '../controllers/shop.controllers.js'
+import { createEditShop, getShop, getShopByCity } from '../controllers/shop.controllers.js'
 import { isAuth } from '../middlewares/isAuth.js'
 import { upload } from '../middlewares/multer.js'
 
 const shopRouter = express.Router()
 shopRouter.post('/create-edit', isAuth, upload.single("image"), createEditShop)
 shopRouter.get('/get-my', isAuth, getShop)
+shopRouter.get('/get-by-city/:city', isAuth, getShopByCity)
 export default shopRouter
