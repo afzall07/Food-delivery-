3ae0a9ffc78518bbeb81f066edf6bad292242e5a
afzall07 <afjalmalik75000@gmail.com>
2025-11-03 22:41:39 +0530

    show total amount

--- frontend/src/pages/CartItem.jsx
+++ frontend/src/pages/CartItem.jsx
@@ -6,7 +6,7 @@ import CartItemCard from "../components/CartItemCard";
 
 function CartItem() {
   const navigate = useNavigate();
-  const { cartItems } = useSelector((state) => state.user);
+  const { cartItems, totalAmount } = useSelector((state) => state.user);
   return (
     <div className="min-h-screen bg-[#fff9f6] flex justify-center p-6">
       <div className="w-full max-w-[800px]">
@@ -25,11 +25,27 @@ function CartItem() {
             Your cart is empty.
           </p>
         ) : (
-          <div>
-            {cartItems?.map((item, index) => (
-              <CartItemCard data={item} key={index} />
-            ))}
-          </div>
+          <>
+            <div className="space-y-4">
+              {cartItems?.map((item, index) => (
+                <CartItemCard data={item} key={index} />
+              ))}
+            </div>
+            <div className="mt-6 bg-white p-4 rounded-xl shadow flex justify-between items-center border">
+              <h1 className="text-lg font-semibold">Total Amount </h1>
+              <span className="text-lg font-bold text-[#ff4d2d]">
+                ₹{totalAmount}
+              </span>
+            </div>
+            <div className="mt-4 flex justify-end">
+              <button
+                className="bg-[#ff4d2d] text-white px-6 py-3 rounded-lg text-lg font-medium hover:bg-[#e64526] transition cursor-pointer"
+                onClick={() => navigate("/checkout")}
+              >
+                Proceed to CheckOut
+              </button>
+            </div>
+          </>
         )}
       </div>
     </div>
