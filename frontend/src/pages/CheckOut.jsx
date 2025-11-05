b7ea8b1a0cd6e8c2b76d4a87cf2488fd6676eaba
afzall07 <afjalmalik75000@gmail.com>
2025-11-05 22:20:55 +0530

    update

--- frontend/src/pages/CheckOut.jsx
+++ frontend/src/pages/CheckOut.jsx
@@ -6,7 +6,12 @@ import { MapContainer, TileLayer, Marker, useMap } from "react-leaflet";
 import { useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import "leaflet/dist/leaflet.css";
-import { setLocation } from "../redux/mapSlice";
+import { setAddress, setLocation } from "../redux/mapSlice";
+import axios from "axios";
+import { useEffect } from "react";
+import { MdDeliveryDining } from "react-icons/md";
+import { FaMobileScreenButton } from "react-icons/fa6";
+import { FaCreditCard } from "react-icons/fa";
 
 function RecenterMap({ location }) {
   if (location.lat && location.long) {
@@ -18,13 +23,54 @@ function RecenterMap({ location }) {
 
 function CheckOut() {
   const { location, address } = useSelector((state) => state.map);
+  const { cartItems } = useSelector((state) => state.user);
+  const [addressInput, setAddressInput] = useState("");
+  const [paymentMethod, setPaymentMethod] = useState("cod");
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const apiKey = import.meta.env.VITE_GEOAPIKEY;
   const onDragEnd = (e) => {
-    console.log(e.target._latlng);
     const { lat, lng } = e.target._latlng;
     dispatch(setLocation({ lat, long: lng }));
+    getAddressByLatLng(lat, lng);
   };
+
+  const getAddressByLatLng = async (lat, lng) => {
+    try {
+      const location = await axios.get(
+        `https://api.geoapify.com/v1/geocode/reverse?lat=${lat}&lon=${lng}&format=json&apiKey=${apiKey}`
+      );
+      dispatch(setAddress(location?.data.results[0].address_line2));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getCurrentLocation = () => {
+    navigator.geolocation.getCurrentPosition(async (position) => {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+      dispatch(setLocation({ lat: latitude, long: longitude }));
+      getAddressByLatLng(latitude, longitude);
+    });
+  };
+
+  const getLatLngByAddress = async () => {
+    try {
+      const result = await axios.get(
+        `https://api.geoapify.com/v1/geocode/search?text=${encodeURIComponent(
+          addressInput
+        )}&apiKey=${apiKey}`
+      );
+      const { lat, lon } = result?.data.features[0].properties;
+      dispatch(setLocation({ lat, long: lon }));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    setAddressInput(address);
+  }, [address]);
   return (
     <div className="min-h-screen bg-[#fff9f6] flex items-center justify-center p-6">
       {/*back icon*/}
@@ -50,12 +96,19 @@ function CheckOut() {
               type="text"
               className="flex-1 border border-gray-300 rounded-lg p-3 text-sm focus:outline-none focus:ring-2 focus:ring-[#ff4d2d]"
               placeholder="Enter Your Location..."
-              value={address}
+              value={addressInput}
+              onChange={(e) => setAddressInput(e.target.value)}
             />
-            <button className="bg-[#ff4d2d] hover:bg-[#e64526] text-white px-3 py-2 rounded-lg flex items-center justify-center">
+            <button
+              className="bg-[#ff4d2d] hover:bg-[#e64526] text-white px-3 py-2 rounded-lg flex items-center justify-center cursor-pointer"
+              onClick={getLatLngByAddress}
+            >
               <IoSearchOutline size={17} />
             </button>
-            <button className="bg-blue-500 hover:bg-blue-600 text-white px-3 py-2 rounded-lg flex items-center justify-center">
+            <button
+              className="bg-blue-500 hover:bg-blue-600 text-white px-3 py-2 rounded-lg flex items-center justify-center cursor-pointer"
+              onClick={getCurrentLocation}
+            >
               <TbCurrentLocation size={17} />
             </button>
           </div>
@@ -80,6 +133,77 @@ function CheckOut() {
                 ></Marker>
               </MapContainer>
             </div>
+          </div>
+        </section>
+
+        {/* payment section  */}
+        <section>
+          <h2 className="text-lg font-semibold mb-3 text-gray-800">
+            Payment method
+          </h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+            {/* cod */}
+            <div
+              className={`flex items-center gap-3 rounded-xl border p-4 text-left transition cursor-pointer ${
+                paymentMethod === "cod"
+                  ? "border-[#ff4d2d] bg-orange-50 shadow"
+                  : "border-gray-200 hover:border-gray-300"
+              }`}
+              onClick={() => setPaymentMethod("cod")}
+            >
+              <span className="inline-flex h-10 w-10 items-center justify-center rounded-full bg-green-100">
+                <MdDeliveryDining className="text-green-600 text-xl" />
+              </span>
+              <div>
+                <p className="font-medium text-gray-800">Cash On Delivery</p>
+                <p className="text-xs text-gray-500">
+                  Pay when your food arrives
+                </p>
+              </div>
+            </div>
+            {/* online */}
+            <div
+              className={`flex items-center gap-3 rounded-xl border p-4 text-left transition cursor-pointer ${
+                paymentMethod === "online"
+                  ? "border-[#ff4d2d] bg-orange-50 shadow"
+                  : "border-gray-200 hover:border-gray-300"
+              }`}
+              onClick={() => setPaymentMethod("online")}
+            >
+              <span className="inline-flex h-10 w-10 items-center justify-center rounded-full bg-purple-100">
+                <FaMobileScreenButton className="text-purple-700 text-lg" />
+              </span>
+              <span className="inline-flex h-10 w-10 items-center justify-center rounded-full bg-blue-100">
+                <FaCreditCard className="text-blue-700 text-lg" />
+              </span>
+              <div>
+                <p className="font-medium text-gray-800">
+                  UPI / Credit / Debit Card
+                </p>
+                <p className="text-xs text-gray-500">Pay Securely Online</p>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        {/* order summary */}
+        <section>
+          <h2 className="text-lg font-semibold mb-3 text-gray-800">
+            Order Summary
+          </h2>
+          <div className="rounded-xl border bg-gray-50 p-4 space-y-2">
+            {cartItems.map((item, index) => (
+              <div
+                key={index}
+                className="flex justify-between text-sm text-gray-700"
+              >
+                <span>
+                  {item.name} x {item.quantity}
+                </span>
+                <span>₹{item.price * item.quantity}</span>
+              </div>
+            ))}
+            <hr className="border-gray-300 my-2" />
           </div>
         </section>
       </div>
