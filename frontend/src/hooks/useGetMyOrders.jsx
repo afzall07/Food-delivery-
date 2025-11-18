8d1aaa0d2b2dccd412e82ab2be63dfb7f29c1bc5
afzall07 <afjalmalik75000@gmail.com>
2025-11-18 23:35:33 +0530

    create useGetMyOrders hook

---
+++ frontend/src/hooks/useGetMyOrders.jsx
@@ -0,0 +1,28 @@
+ import React, { useEffect } from "react";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { setMyOrders } from "../redux/userSlice";
+
+function useGetMyOrders() {
+  const dispatch = useDispatch();
+  const { userData } = useSelector((state) => state.user);
+  useEffect(() => {
+    const fetchOrders = async () => {
+      try {
+        const result = await axios.get(
+          "http://localhost:7000/api/order/my-orders",
+          {
+            withCredentials: true,
+          }
+        );
+          dispatch(setMyOrders(result.data));
+          console.log(result.data)
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchOrders();
+  }, [dispatch, userData]);
+}
+
+export default useGetMyOrders;
