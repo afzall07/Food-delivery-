0b70fccd32c91fdd4b10496d89e428f634668a99
afzall07 <afjalmalik75000@gmail.com>
2025-11-18 23:38:34 +0530

    create MyOrders page

---
+++ frontend/src/pages/MyOrders.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { IoIosArrowRoundBack } from "react-icons/io";
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom'
+import UserOrderCard from '../components/UserOrderCard';
+import OwnerOrderCard from '../components/OwnerOrderCard';
+function MyOrders() {
+    const { userData, myOrders } = useSelector(state => state.user)
+    const navigate = useNavigate()
+    return (
+        <div className='w-full min-h-screen bg-[#fff9f6] flex justify-center px-4'>
+            <div className='w-full max-w-[800px] p-4'>
+                <div className="flex items-center gap-[20px] mb-6">
+                    {/*back icon*/}
+                    <div className=" z-[10]" onClick={() => navigate("/")}>
+                        <IoIosArrowRoundBack
+                            size={35}
+                            className="text-[#ff4d2d] cursor-pointer"
+                        />
+                    </div>
+                    <h1 className="text-2xl font-bold text-start">My Orders</h1>
+                </div>
+                <div className="space-y-6">
+                    {myOrders?.map((order, index) => (
+                        userData.role == "user" ? (
+                            <UserOrderCard data={order} key={index} />
+
+                        ) :
+                            userData.role == "owner" ? (
+                                <OwnerOrderCard data={order} key={index} />
+                            ) : null
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default MyOrders
