9b3baec3c329c0f9191a4724cb5dc41af504f8ef
afzall07 <afjalmalik75000@gmail.com>
2025-11-26 21:55:10 +0530

    add addMyOrder/updateOrderStatus reducer

--- frontend/src/redux/userSlice.js
+++ frontend/src/redux/userSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { act } from "react";
 
 const userSlice = createSlice({
     name: "user",
@@ -11,7 +12,7 @@ const userSlice = createSlice({
         itemsInMyCity: null,
         cartItems: [],
         totalAmount: 0,
-        myOrders:null
+        myOrders:[]
     },
     reducers: {
         setUserData: (state, action) => {
@@ -57,9 +58,21 @@ const userSlice = createSlice({
         },
         setMyOrders: (state, action) => {
             state.myOrders=action.payload
+        },
+        addMyOrder: (state, action) => {
+            state.myOrders = [action.payload, ...state.myOrders]
+        },
+        updateOrderStatus: (state, action) => {
+            const { orderId, shopId, status } = action.payload
+            const order = state.myOrders.find(o => o._id == orderId)
+            if (order) {
+                if (order.shopOrders && order.shopOrders.shop._id==shopId) {
+                 order.shopOrders.status=status   
+                }
+            }
         }
 
     }
 })
-export const { setUserData, setCurrentCity, setCurrentState, setCurrentAddress, setShopsInMyCity, setItemsInMyCity, addToCart, updateQuantity, removeCartItem , setMyOrders} = userSlice.actions
+export const { setUserData, setCurrentCity, setCurrentState, setCurrentAddress, setShopsInMyCity, setItemsInMyCity, addToCart, updateQuantity, removeCartItem , setMyOrders, addMyOrder,updateOrderStatus} = userSlice.actions
 export default userSlice.reducer
