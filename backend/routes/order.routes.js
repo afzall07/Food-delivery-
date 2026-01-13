5cc305f162b32bef5507c79eb87394d7ad85d0c4
afzall07 <afjalmalik75000@gmail.com>
2026-01-13 21:47:40 +0530

    create acceptOrder route

--- backend/routes/order.routes.js
+++ backend/routes/order.routes.js
@@ -1,11 +1,14 @@
 import express from 'express'
 import { isAuth } from '../middlewares/isAuth.js'
-import { getDeliveryBoyAssignments, getMyOrders, placeOrder, updateOrderStatus } from '../controllers/order.controllers.js'
+import { acceptOrder, getCurrentOrder, getDeliveryBoyAssignments, getMyOrders, getOrderById, placeOrder, updateOrderStatus } from '../controllers/order.controllers.js'
 
 const orderRouter = express.Router()
 orderRouter.post('/place-order', isAuth, placeOrder)
 orderRouter.get('/my-orders', isAuth, getMyOrders)
 orderRouter.get('/get-assignments', isAuth, getDeliveryBoyAssignments)
+orderRouter.get('/get-current-order', isAuth, getCurrentOrder)
 orderRouter.post('/update-status/:orderId/:shopId', isAuth, updateOrderStatus)
+orderRouter.get('/accept-order/:assignmentId', isAuth, acceptOrder)
+orderRouter.get('/get-order-by-id/:orderId', isAuth, getOrderById)
 
 export default orderRouter
