3c8e503fa54f32679a0ebdc4028823bb3433a42d
afzall07 <afjalmalik75000@gmail.com>
2026-01-14 23:29:11 +0530

    update

--- frontend/src/pages/TrackOrderPage.jsx
+++ frontend/src/pages/TrackOrderPage.jsx
@@ -41,7 +41,7 @@ function TrackOrderPage() {
                             <p className='font-semibold'><span>Delivery Boy conntact No: </span>{shopOrder.assignedDeliveryBoy.mobile} </p>
                         </div> : <p className='font-semibold'>Delivery Boy is not assigned yet.</p>}
                     </> : <p className='text-green-600 font-semibold text-lg'>Delivered</p>}
-                    {shopOrder.assignedDeliveryBoy && <div className='h-[400px] w-full rounded-2xl overflow-hidden shadow-md'>
+                    {(shopOrder.assignedDeliveryBoy && shopOrder.status !== "delivered") && <div className='h-[400px] w-full rounded-2xl overflow-hidden shadow-md'>
                         <DeliveryBoyTracking data={{
                             deliveryBoyLocation: {
                                 lat: shopOrder.assignedDeliveryBoy.location.coordinates[1],
