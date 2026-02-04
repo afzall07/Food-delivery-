e1dc773128dbbd45e74b7e88df8d39d677bd0c8e
afzall07 <afjalmalik75000@gmail.com>
2026-02-04 22:42:46 +0530

    update delivery boy location through socket io

--- frontend/src/components/DeliveryBoy.jsx
+++ frontend/src/components/DeliveryBoy.jsx
@@ -10,7 +10,42 @@ function DeliveryBoy() {
   const [currentOrder, setCurrentOrder] = useState();
   const [availableAssignments, setAvailableAssignments] = useState(null)
   const [showOtpBox, setShowOtpBox] = useState(false)
-  const [otp, setOtp] = useState("")
+  const [otp, setOtp] = useState("");
+  const [deliveryBoyLocation, setDeliveryBoyLocation] = useState({
+    lat: null,
+    lon: null
+  })
+
+  useEffect(() => {
+    if (!socket || userData.role !== "deliveryBoy") return;
+    let watchId;
+    if (navigator.geolocation) {
+      watchId = navigator.geolocation.watchPosition((position) => {
+        const latitude = position.coords.latitude
+        const longitude = position.coords.longitude
+        setDeliveryBoyLocation({
+          lat: latitude,
+          lon: longitude
+        })
+        socket.emit("updateLocation", {
+          latitude,
+          longitude,
+          userId: userData._id
+        });
+      }),
+        (error) => {
+          console.log(`error ${error}`)
+        },
+      {
+        enableHighAccuracy: true
+      }
+    };
+    return () => {
+      if (watchId) navigator.geolocation.clearWatch(watchId)
+    }
+  }, [socket, userData])
+
+
   const getAssignments = async () => {
     try {
       const result = await axios.get(`${serverUrl}/api/order/get-assignments`, { withCredentials: true })
@@ -24,7 +59,6 @@ function DeliveryBoy() {
     try {
       const result = await axios.get(`${serverUrl}/api/order/get-current-order`, { withCredentials: true })
       setCurrentOrder(result.data)
-      console.log(`currentOrder ${result.data}`)
     } catch (error) {
       console.log(`getCurrentOrder error ${error}`)
     }
@@ -83,7 +117,7 @@ function DeliveryBoy() {
       <div className="w-full max-w-[800px] flex flex-col gap-5 items-center mt-20 p-2">
         <div className="bg-white rounded-2xl shadow-md p-5 flex flex-col justify-start items-center w-full border border-orange-100 gap-2 text-center">
           <h1 className="text-xl font-bold text-[#ff4d2d]">Welcome, {userData.fullName}</h1>
-          <p className="text-[#ff4d2d]"><span className="font-semibold">Latitude: </span>{userData.location.coordinates[1]},  <span className="font-semibold">Longitude: </span>{userData.location.coordinates[0]}</p>
+          <p className="text-[#ff4d2d]"><span className="font-semibold">Latitude: </span>{deliveryBoyLocation?.lat},  <span className="font-semibold">Longitude: </span>{deliveryBoyLocation?.lon}</p>
         </div>
         {!currentOrder && <div className="bg-white rounded-2xl p-5 shadow-md w-[90%] border border-orange-100">
           <h1 className="text-lg font-bold mb-4 flex items-center gap-2">Available Orders</h1>
@@ -113,7 +147,16 @@ function DeliveryBoy() {
               <p className="text-sm text-gray-500">{currentOrder.deliveryAddress.text}</p>
               <p className="text-xs text-gray-400">{currentOrder.shopOrder.shopOrderItems.length} items | ₹{currentOrder.shopOrder.subTotal}</p>
             </div>
-            <DeliveryBoyTracking data={currentOrder} />
+            <DeliveryBoyTracking data={{
+              deliveryBoyLocation: deliveryBoyLocation || {
+                lat: userData.location.coordinates[1],
+                lon: userData.location.coordinates[0],
+              },
+              customerLocation: {
+                lat: currentOrder.deliveryAddress.latitude,
+                lon: currentOrder.deliveryAddress.longitude,
+              },
+            }} />
             {!showOtpBox ? <button className="mt-4 w-full bg-green-500 text-white font-semibold py-2 px-4 rounded-xl shadow-md hover:bg-green-600 active:scale-95 transition-all duration-200" onClick={sendOtp}>Mark as Delivered</button> : <div className="mt-4 p-4 border rounded-xl bg-gray-50">
               <p className="text-sm font-semibold mb-2">Enter OTP send to <span className="text-[#ff4d2d]">{currentOrder.user.fullName}</span></p>
               <input type="text" className="w-full border px-3 py-2 rounded-lg mb-3 focus:outline-none focus:ring-2 focus:ring-orange-400" placeholder="Enter OTP" onChange={(e) => setOtp(e.target.value)} value={otp} />
