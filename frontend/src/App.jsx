68d2db17e0d12a89b43947940ded3e5cc2e5c1ad
afzall07 <afjalmalik75000@gmail.com>
2026-02-08 20:48:47 +0530

    Update server URL to production endpoint

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,7 +24,7 @@ import Shop from "./pages/Shop";
 import { useEffect } from "react";
 import { io } from "socket.io-client";
 import { setSocket } from "./redux/userSlice";
-export const serverUrl = "http://localhost:7000"
+export const serverUrl = "https://khaopio-backend.onrender.com"
 
 function App() {
   const { userData } = useSelector((state) => state.user);
